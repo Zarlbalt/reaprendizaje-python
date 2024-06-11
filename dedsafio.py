901da63ed5557f8f18fef8d63a36751c994c0d41
Zarlbalt <zarlbalt2@gmail.com>
2024-06-11 03:28:56 -0600

    commit pruebas 2

--- dedsafio.py
+++ dedsafio.py
@@ -1,7 +1,6 @@
 import random
 
 def obtener_eleccion_jugador():
-<<<<<<< HEAD
     eleccion = input("piedra, papel o tijera").lower()
     while eleccion not in ["piedra", "papel" , "tijera"]:
         print("entrada invalida")
@@ -11,16 +10,3 @@ def obtener_eleccion_jugador():
 def obtener_eleccion_computador():
     opciones = ["piedra","papel","tijera"]
     eleccion = random.choice(opciones)
-=======
-    eleccion = input("Elige piedra, papel o tijera: ").lower()
-    while eleccion not in ["piedra", "papel", "tijera"]:
-        print("Entrada inválida, por favor elige nuevamente.")
-        eleccion = input("Elige piedra, papel o tijera: ").lower()
-    return eleccion
-
-def obtener_eleccion_computadora():
-    opciones = ["piedra", "papel", "tijera"]
-    eleccion = random.choice(opciones)
-    return eleccion
-
->>>>>>> origin/main
