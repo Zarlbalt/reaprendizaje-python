aecbbff1936a519cc6bdaefa85553fec0ac39470
Zarlbalt <zarlbalt2@gmail.com>
2024-06-14 00:42:57 -0600

    se agrego la version de congunto de nombre y apellido completo, para poder ver las diferencias

--- tipos/03-format-strings.py
+++ tipos/03-format-strings.py
@@ -3,3 +3,5 @@ apellido = "Baltazar"
 
 nombre_completo = f"{nombre[0]} {2+5}"  # siempre y cuando las llaves esten dentro de un operador de formateo de string, podemos escribir la expresion que queramos
 print(nombre_completo)
+nombre_completo = f"{nombre} {apellido}"
+print(nombre_completo)
