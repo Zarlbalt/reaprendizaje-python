addaa1b9dbaa47d55ea54b42035c3092b2539654
Zarlbalt <zarlbalt2@gmail.com>
2024-06-14 01:22:07 -0600

    secuencias de escape y con modelos letras especiales

---
+++ tipos/05-secuencias-escape.py
@@ -0,0 +1,9 @@
+# curso = 'Ultimate "Python"'
+curso2 = "Ultimate \n\\Python\\"
+# print(curso)
+print(curso2)
+
+# \"   indica que las comillas son especiales y el codigo entiende que no es para lo que se usa principalmente en el codigo
+# \'
+# \\ si queremos mostrar un backslash de esta forma se deberia mostrar
+# \n  hace como si fuera un enter pero en la terminal
