592932eb9c9ff5d9eb5e2c264191f7086e029f7c
Zarlbalt <zarlbalt2@gmail.com>
2024-06-14 00:41:30 -0600

    se trabajo con los formatos de string y de que cuando se utiliza operadores de formato podemos utilizar expresiones dentro las llaves

---
+++ tipos/03-format-strings.py
@@ -0,0 +1,5 @@
+nombre = "Carlos"
+apellido = "Baltazar"
+
+nombre_completo = f"{nombre[0]} {2+5}"  # siempre y cuando las llaves esten dentro de un operador de formateo de string, podemos escribir la expresion que queramos
+print(nombre_completo)
