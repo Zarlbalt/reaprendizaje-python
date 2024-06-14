730b1a394d2c214e724299ade82f33f70ad642ef
Zarlbalt <zarlbalt2@gmail.com>
2024-06-14 00:07:26 -0600

    variedad de tipos de nombre que se pueden poner en python y los tipos de variable como int, float y bolean

--- tipos/variable.py
+++ tipos/variable.py
@@ -1,2 +1,11 @@
 # no se puede crear variables con espacios y no pueden iniciar con un numero, pero se pueden agregar numeros una variable.
 nombre_curso = "Python"
+nombre1 = "ultime python"
+NOMBRE_CURSO = "PYTHON"
+NoMbRe_CURSO = "ULTIMATE"
+Nombre_Curso = "Feliz"
+print(nombre_curso, nombre1, NOMBRE_CURSO, NoMbRe_CURSO, Nombre_Curso)
+
+alumnos = 5000  # numero entero
+puntaje = 9.9  # numero Float
+publicado = False  # booleano (bolean)  True = si , False = no.
