930b06e759eeb5a9e2bad74d154fe8974dc66b5d
Zarlbalt <zarlbalt2@gmail.com>
2024-06-14 21:57:57 -0600

    vi funciones que se pueden utilizar para tipicos problemas matematicos

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,15 @@
+import math  # modulo que tiene codigo python que se encuentra en otro archivo asi seria el ejemplo
+
+print(round(1.3))  # devuelve el numero en el que se encuentre mas cercano
+print(round(1.7))
+print(round(1.5))
+print(abs(-77))  # valor absoluto
+print(abs(55))
+
+print(math.ceil(1.1))
+print(math.floor(1.9999))
+print(math.isnan(23))  # el argumento que le tenemos que dar es numero real
+print(math.pow(10, 3))  # sirve para elevar un numero
+print(math.sqrt(9))  # sacar raiz cuadrada de un numero
+
+# https://docs.python.org/3/library/math.html
