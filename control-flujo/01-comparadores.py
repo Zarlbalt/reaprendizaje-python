5d6e1e871e5eeef81f700a4ca224c54f9912f74d
Zarlbalt <zarlbalt2@gmail.com>
2024-06-15 01:51:44 -0600

    vi todos los comparadores logicos

---
+++ control-flujo/01-comparadores.py
@@ -0,0 +1,11 @@
+# una expresion es una cadena de instrucciones que despues se van a evaluar en una sola cosa
+print(1 > 2)  # cuando se utiliza estos comparadores siempre daran de regreso un true o un false
+print(1 < 2)
+print(1 >= 2)
+print(1 <= 2)
+print(2 >= 2)
+print(2 <= 2)
+print(2 == 2)
+print(2 == 3)
+print(2 == "2")#como son tipo de datos distintos devuelve false
+print(2 != "2") #no es igual es lo que significa
