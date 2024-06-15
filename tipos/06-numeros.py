de86d37dffef7a44a2aa6899922912fe4ee01e64
Zarlbalt <zarlbalt2@gmail.com>
2024-06-14 21:47:48 -0600

    vi los tipos de numeros int,float e imaginario, y tambien se vio como se remplasan las variables por si mismas y algunas formas de que los resultados se nos muestre de una forma distinta y las operaciones basicas que se pueden hacer en una computadora y en especial modulo que sera util para los problemas de par o impar

---
+++ tipos/06-numeros.py
@@ -0,0 +1,22 @@
+numero = 2  # entero -> integer
+decimal = 1.2  # float
+imaginario = 2 + 2j  # 2 + 2i
+
+numero = numero + 2  # la estoy replasando por si misma
+numero += 2  # sirve para la misma cosa que la de arriba solo que simplificado
+# este tipo de simplificacion sirve para todos los operadores de abajo
+
+numero += 2
+numero -= 2
+numero *= 2
+numero /= 2
+# hasta aqui
+
+print("numero", numero)
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 // 3)  # el doble es cuando no queremos que nos muestre los decimales
+print(8 % 3)  # conocido como modulo pero que lo que da es lo que sobra de la divicion
+print(2**3)  # sirve para elever un numero como representacion seria  2^3 en calculadora
