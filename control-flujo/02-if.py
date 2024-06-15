0b02c9a5eeb2439b9773dff0f7e1c28e3f955e0a
Zarlbalt <zarlbalt2@gmail.com>
2024-06-15 02:02:45 -0600

    vi las condiciones que se pueden hacer con if elif y else

---
+++ control-flujo/02-if.py
@@ -0,0 +1,11 @@
+edad = 22
+if edad > 54:
+    print("Puede ver la pelicula con descuento")
+elif edad > 17:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes entrar")
+
+print("Listo")
+
+# mantener el orden de las expresiones en el if
